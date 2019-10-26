6a4b07aef9f35b502fec8806efe9758736131c2e
Zeeshan Abbasi <48386803+TechieZee@users.noreply.github.com>
2019-10-26 19:14:15 +0530

    Add files via upload
    Please contribute in it and make it more amazing with GUI.

---
+++ SPS_game.py
@@ -0,0 +1,41 @@
+import random
+# Taking input from the user
+user_input = input("Enter any of the choices- s(Stone O) p(Paper ===) c(Scissor >8)")
+if user_input.lower() == 's':
+    print("O  vs",end='  ')
+elif user_input.lower() == 'p':
+    print("=== vs",end='  ')
+elif user_input.lower() == 'c':
+    print(">8 vs",end='  ')
+else:
+    print("Option selected is not correct!!")
+
+# Taking input from the computer using random function
+comp_selection = random.randint(1,3)
+if comp_selection == 1:
+    comp='s'
+    print("O")
+elif comp_selection == 2:
+    comp='p'
+    print("===")
+else:
+    comp='c'
+    print(">8")
+
+# Winning Logic
+if user_input.lower() == comp.lower():
+    print("Draw!")
+elif user_input.lower() == 's' and comp.lower() =='p':
+    print("Computer Wins!")
+elif user_input.lower() == 's' and comp.lower() =='c':
+    print("Player Wins!")
+elif user_input.lower() == 'p' and comp.lower() =='c':
+    print("Computer Wins!")
+elif user_input.lower() == 'p' and comp.lower() =='s':
+    print("Player Wins!")
+elif user_input.lower() == 'c' and comp.lower() =='s':
+    print("Computer Wins!")
+elif user_input.lower() == 'c' and comp.lower() =='p':
+    print("Player Wins!")
+
+
---
+++ graphics.py
@@ -0,0 +1,6 @@
+from tkinter import *
+screen =  Tk()
+screen.geometry("600x600")
+screen.title("Stone Paper Scissor Game")
+Heading = Label(text = "Stone O ---> Paper == ---> Scissor >8", fg = "red", bg = "black")
+Heading.pack()
